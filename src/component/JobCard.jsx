8390f66b573ffa5e5ac9d6286cbddd098d716ccc
Srishti Gautam <82943455+srishtigautam21@users.noreply.github.com>
2024-03-15 12:15:16 +0530

    ui change

--- src/component/JobCard.jsx
+++ src/component/JobCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import companyImg from "../assets/appleimg.png";
+import { CircularProgressbar } from "react-circular-progressbar";
 
 const JobCard = ({ job }) => {
   return (
@@ -11,20 +12,70 @@ const JobCard = ({ job }) => {
           style={{ objectFit: "cover", width: "60px" }}
         />
         <div>
-          <div className='font-bold text-gray-400'>{job.jobTitle}</div>
+          <div className='font-bold text-gray-200'>{job.jobTitle}</div>
           <div className='flex flex-col'>
-            <span className='text-gray-500'>{job.companyName}</span>
-            <span className='text-gray-500'>{job.location}</span>
+            <span className='text-gray-200'>{job.companyName}</span>
+            <span className='text-gray-200'>{job.location}</span>
           </div>
         </div>
-        {/* <div className='font-bold text-gray-400'>Skills match</div> */}
+        <div className='flex gap-7 items-center justify-center'>
+          <div className='font-bold text-base text-[#FFFFFF]'>Skill Match</div>
+          <CircularProgressbar
+            value={percentage}
+            text={`${percentage}%`}
+            className='w-20 h-20'
+            styles={{
+              // Customize the root svg element
+              root: {},
+
+              // Customize the path, i.e. the "completed progress"
+              path: {
+                // Path color
+                stroke: `rgb(64, 162, 227, ${percentage / 100})`,
+
+                // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+                strokeLinecap: "butt",
+                // Customize transition animation
+                // transition: 'stroke-dashoffset 0.5s ease 0s',
+                // Rotate the path
+                // transform: 'rotate(0.25turn)',
+                transformOrigin: "center center",
+              },
+              // Customize the circle behind the path, i.e. the "total progress"
+              trail: {
+                // Trail color
+                stroke: "#E83363",
+                // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+                strokeLinecap: "butt",
+                // Rotate the trail
+                transform: "rotate(0.25turn)",
+                transformOrigin: "center center",
+              },
+              // Customize the text
+              text: {
+                // Text color
+                fill: "#FFFFFF",
+                // Text size
+                fontSize: "20px",
+                fontWeight: "600",
+              },
+              // Customize background - only used when the `background` prop is true
+              background: {
+                fill: "#E83363",
+              },
+            }}
+          />
+        </div>
       </div>
+
+      {/* <div className='font-bold text-gray-400'>Skills match</div>
+      </div> */}
       <div className='bg-cardFooter h-[60px] p-3 rounded-bl-xl rounded-br-xl flex items-center justify-between'>
-        <div className='text-gray-400'>
+        <div className='text-gray-200'>
           <span>Posted {job.datePosted} | </span>
           <span> 10 applicants</span>
         </div>
-        <button className='w-[150px] h-[36px] bg-teal-600 text-white rounded-2xl'>
+        <button className='w-[150px] h-[36px] bg-teal-600 text-white rounded-2xl hover:cursor-pointer hover:bg-teal-400'>
           Apply now
         </button>
       </div>
--- src/component/JobList.jsx
+++ src/component/JobList.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import JobCard from "./JobCard";
 import { useFilterHook } from "../utils/useFilterHook";
 import { useData } from "../context/dataContext";
+import { RiArrowDropDownLine } from "react-icons/ri";
 
 const JobList = () => {
   const { jobList, setJobList } = useData();
@@ -16,7 +17,10 @@ const JobList = () => {
         </div>
         <div className='flex gap-3 items-center'>
           <span className='text-white'>Sort By</span>
-          <button className='w-[100px] h-[35px] bg-white'>Latest</button>
+          <div className='w-[100px] h-[35px] flex items-center justify-center gap-2 text-[#333333] bg-gray-200 rounded-sm p-2'>
+            <button>Latest</button>
+            <RiArrowDropDownLine />
+          </div>
         </div>
       </div>
       <div className='bg-joblist w-[900px] p-5 flex flex-col items-center justify-center gap-5 rounded-xl'>
